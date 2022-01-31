6349425bcd26340d208417b8890ca80d6dcef878
viacrucisapp <viacrucisapp@gmail.com>
2022-01-31 19:29:38 -0300

    Home slider edit

---
+++ src/components/SliderCard.tsx
@@ -0,0 +1,43 @@
+import { useEffect }  from 'react';
+import { IonCard, IonCardContent } from '@ionic/react';
+import { useTranslation } from "react-i18next"
+import './SliderCard.css'
+import { Ilectures, Iimages} from "../common/types";
+
+type LectureCardProps = {
+    lecture: Ilectures;
+    images: Iimages[],
+
+}
+
+
+export const SliderCard = ({lecture, images} : LectureCardProps) => {
+
+    const [t, i18n] = useTranslation("lectures");
+    
+    useEffect(() => {
+        // Here goes the code you wish to run on mount
+        console.log(window.screen.width)
+        return () => {
+          // Here goes the code you wish to run on unmount
+        }
+      }, []);
+    
+      return (
+        <IonCard button style={{backgroundImage :`url(${(images[lecture.id].small)}`}}  class="slider-card" mode="ios"  routerLink={`/lectures/${lecture.title}`} >
+            
+                
+    
+                
+                    <IonCardContent class="slider-card__content">
+                        <h2 className="slider-card__title">{t(lecture.title)}</h2> 
+                        <h3 color="primary" className="slider-card__text">{t(lecture.subTitle)}</h3>
+                    </IonCardContent>
+                
+            
+            
+        </IonCard>
+        
+    )
+}
+
